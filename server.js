19850aa73902afeffada3255a7d2a6352e5f1e9d
Ashley Allphin <ashley.allphin@gmail.com>
2020-07-08 01:57:20 -0400

    should the log in page hava twitchy mustache GIF? idk.

--- server.js
+++ server.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-// require dependencies
-var express = require('express');
-const path = require('path');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var app = express();
-const mongoose = require('mongoose');
-var PORT = process.env.PORT || 5000
-// invoke config method for dotenv
-require('dotenv').config();
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-	);
-
-// use mongoose to connect to the Mongo database
-const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-ge9yd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-mongoose
-	.connect(
-		mongoURI,
-		{ useNewUrlParser: true, useUnifiedTopology: true } 
-	)
-	.then(() => console.log(`Successfully connected to MongoDB.` + 
-	`\n-----------------------------------------------------`))
-	.catch(err => console.log(err))
-
-// set static directory to build folder
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// use routes from /routes
-var Users = require('./routes/Users')
-var Projects = require('./routes/Projects')
-app.use('/users', Users)
-app.use('/', Projects)
-
-// listen on the port
-app.listen(PORT, () => {
-    console.log(`\nApp listening on http://localhost:${PORT}`);
-=======
 // VARIABLES
 // ================================================
 const color = require('colors-cli/toxic');
@@ -98,5 +54,4 @@ app.use(function (err, req, res, next) {
 var PORT = 8080
 app.listen(PORT, () => {
     console.log(`\nApp listening on `.x81 + `http://localhost:${PORT}`.x226.underline);
->>>>>>> repo-2/master
 });
